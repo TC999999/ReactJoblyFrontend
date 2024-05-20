b667b2919eb88816e43a4161dfc3bc35f8c618c4
Tyler <insertcleverusernamehere999@gmail.com>
2024-05-20 14:57:11 -0400

    trying again with dotenv

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
